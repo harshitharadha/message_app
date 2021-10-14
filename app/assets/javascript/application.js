f54209ebe260142c22c89d02bc742fbc288aa11f
Harshitha S M <harshithaharshu98845@gmail.com>
2021-10-14 19:10:32 +0530

    Enable flash messages

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -8,4 +8,7 @@
 
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
 })
