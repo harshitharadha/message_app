cb9d52137068dd3647bb51c0354c37a9bce1a8f8
Harshitha S M <harshithaharshu98845@gmail.com>
2021-10-14 15:47:27 +0530

    Add users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "harshu1", password: "password1")
+User.create(username: "harshu2", password: "password2")
+User.create(username: "harshu3", password: "password3")
+User.create(username: "harshu4", password: "password4")
+User.create(username: "harshu5", password: "password5")
